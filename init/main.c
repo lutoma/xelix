3242a098c0d8f74469d23c809ee58615479c245b
Lukas Martini <lukas.martini@junge-piraten.de>
2010-08-05 00:48:29 +0200

    CPU stuff updated

--- init/main.c
+++ init/main.c
@@ -10,23 +10,15 @@ void kmain()
 	*bildschirm = 0x0f << 8 | 'a';
 	
 	display_init();
-	display_print(":: Display initialisiert.\n");
+	display_print("Display initialisiert.\n");
 	
-	display_print(":: Selbstest ob ints richtige Laengen haben.\n");
+	display_print("Selbstest ob ints richtige Laengen haben.\n");
 	if(sizeof(uint8) == 1)
 		display_print("     uint8 richtig!\n");
 	if(sizeof(uint16) == 2)
 		display_print("     uint16 richtig!\n");
 	if(sizeof(uint32) == 4)
 		display_print("     uint32 richtig!\n");
-	
-	
-	uint32 v;
-	asm ( "mov %%cr0, %0":"=a"(v) );
-	display_print("sr0: ");
-	display_printHex(v);
-	if(v & 1)
-		display_print("-> protected mode already enabled");
 	
 
 
