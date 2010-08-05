0038de9a085870d7bbd6a4a4fb81a5b66b5ee172
Lukas Martini <lukas.martini@junge-piraten.de>
2010-08-05 11:33:57 +0200

    Added script for counting lines.

---
+++ tools/linecount.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+NUMBER=0
+LINE=0
+CHAR=0
+count ()
+{
+	for temp in $1/* ; do
+		if [ -d "$temp" ] ; then
+			count "$temp"
+		elif [ -f "$temp" ] ; then
+			NUMBER=$(($NUMBER+1))
+			LINE=$(($LINE+`wc -l $temp | awk '{print $1}'`))
+			CHAR=$(($LINE+`wc -m $temp | awk '{print $1}'`))
+		fi
+	done
+}
+count $1
+echo "Found $NUMBER files containing $LINE lines and $CHAR characters"
