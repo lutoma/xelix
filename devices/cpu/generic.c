f83aa44049389224af946555000c408e0b596b99
Lukas Martini <lukas.martini@junge-piraten.de>
2010-08-07 15:43:19 +0200

    Updated to new format

--- devices/cpu/generic.c
+++ devices/cpu/generic.c
@@ -29,7 +29,7 @@ int isProtected()
 // Not needed so far, as grub automatically switches to protected mode.
 void setProtected()
 {
-	panic("Can't [yet] switch to protected mode. Use a bootloader which automatically enables protected mode such as GNU GRUB.\n");
+	PANIC("Can't [yet] switch to protected mode. Use a bootloader which automatically enables protected mode such as GNU GRUB.\n");
 	return;
 }
 
