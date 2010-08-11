6e37e68109921fefd1132a7d8cc9ceef0d6fd580
Christoph Sünderhauf <chris@sunderhauf.net>
2010-08-12 01:54:44 +0200

    added process_t

--- memory/paging/paging.c
+++ memory/paging/paging.c
@@ -4,7 +4,7 @@
 #include <interrupts/interface.h>
 
 /////////////////////////////
-// STRUCTURES
+// ENUMS
 
 enum mode {
 	USER_MODE = 1,
@@ -15,52 +15,6 @@ enum readandwrite {
 	READONLY = 0,
 	READWRITE = 1
 };
-	
-
-typedef struct {
-	uint32 present  :1;   // Page present in memory 
-	uint32 rw       :1;   // Read-only if clear, readwrite if set (only applies to code running in user-mode, kernel-mode can do everything ;) )
-	uint32 usermode :1;   // Supervisor level only if clear
-	uint32 w        :1;   // write through - set: write-though caching, unset:write back
-	uint32 cachedis :1;   // cache disabled. set: page won't be cached
-	uint32 accessed :1;   // Has the page been accessed since last refresh?
-	uint32 dirty    :1;   // Has the page been written to since last refresh?
-	uint32 zero     :1;   // always zero
-	uint32 global   :1;   // something complicated - leave it disabled ;)
-	uint32 available:3;   // free for our use
-	uint32 frame    :20;  // physical frame address (shifted right 12 bits->aligned to 4kb), ie. frame number
-} pageTableEntry_t; // describes a page
-
-typedef struct {
-	pageTableEntry_t tableEntries[1024];
-} pageTable_t;
-
-typedef struct {
-	uint32 present  :1;   // Page present in memory 
-	uint32 rw       :1;   // Read-only if clear, readwrite if set 
-	uint32 usermode :1;   // Supervisor level only if clear
-	uint32 w        :1;   // write through - set: write-though caching, unset:write back
-	uint32 cachedis :1;   // cache disabled. set: page won't be cached
-	uint32 accessed :1;   // Has the page been accessed since last refresh?
-	uint32 zero     :1;   // always zero
-	uint32 pagesize :1;   // 0 for 4kb
-	uint32 ignored  :1;
-	uint32 available:3;   // free for our use
-	uint32 pagetable:20;  // physical address of page table (shifted right 12 bits->aligned to 4kb)
-} pageDirectoryEntry_t; // points to a page table
-
-typedef struct {
-	pageDirectoryEntry_t directoryEntries[1024];
-	
-	// the cpu only needs a the physical address to above directoryEntries, so
-	// the page directory on a hardware level just consists of the upper
-	// directoryEntries.
-	// but we need to store some additional meta-information:
-	
-	pageTable_t* pageTables[1024]; // direct pointers (i.e. virtual addresses) to the page tables, because the directoryEntries contain the physical address of the page tables and we need virtual addresses to access them in the kernel. If the present bit in the corresponding directoryEntries[i] is 0, then pageTables[i] be zero.
-	
-	uint32 physicalAddress; // the physical address of the directoryEntries[] array, because we need it to give it to the cpu.
-} pageDirectory_t;
 
 /////////////////////////////
 // VARIABLES
