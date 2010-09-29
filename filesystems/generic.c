7aad92158fd45181e80f76321d585ee99d79e923
Lukas Martini <lukas.martini@junge-piraten.de>
2010-09-29 02:16:19 +0200

    Remove filesystem stuff as it is posix-incompatible

--- filesystems/generic.c
+++ filesystems/generic.c
@@ -3,50 +3,12 @@
 #include <filesystems/interface.h>
 #include <memory/kmalloc.h>
 
-// Read contents of a node
-char* fs_read(node_t* node)
-{
-	if(node->readFunction <= 0) return;
-	(*node->readFunction) (node);
-	return "foo";
-}
 
-// Write to a specific node
-void fs_write(node_t* node, char* what)
-{
-
-}
-
-// Show contents of directory
-// buffer A buffer array to write to
-// returns array with file-nodes
-// Only supports 50 items [50]
-//
-node_t* fs_list(node_t* node, node_t buffer[])
-{
-
-}
-
-// Search for a specific node by filename
-// returns found node
-//
-node_t* fs_search(node_t* start, char* name)
-{
-
-}
 
 
 // Initialize the filesystem abstraction system
 void fs_init()
 {
-	fs_root = kmalloc(sizeof(node_t));
-	log("Position of filesystem root node in memory: ");
-	logHex(fs_root);
-	log("\n");
-
-	ASSERT(fs_root > 0); // Now check for fs_root
-
-	fs_root->isDir = 1;
 
 	log("Initialized filesystem abstraction\n");
 }
--- filesystems/interface.h
+++ filesystems/interface.h
@@ -1,25 +1,3 @@
 #pragma once
 
 #include <common/generic.h>
-
-typedef struct node {
-	uint32 inode;
-	char* name;
-	struct node* parent;
-	char* (*readFunction)(struct node*);
-	void (*writeFunction)(struct node*, char*);
-	struct node* (*listFunction)(struct node*, struct node*);
-	struct node* (*searchFunction)(struct node*, char*);
-	int mountPoint;
-	int isDir;
-	char* fsType;
-} node_t;
-
-/// The root directory /.
-node_t* fs_root;
-
-char* fs_read(node_t* node);
-void fs_write(node_t* node, char* what);
-node_t* fs_list(node_t* node, node_t buffer[]);
-node_t* fs_search(node_t* start, char* name);
-void fs_init();
