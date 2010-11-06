c399592c46b345640a1cfbbbf6c16943a6646f6c
Lukas Martini <lukas.martini@junge-piraten.de>
2010-11-06 03:49:43 +0100

    Increase standard PIT rate

--- common/stdconf.h
+++ common/stdconf.h
@@ -2,11 +2,12 @@
 
 #pragma once
 
-#define PIT_RATE 50
+#define PIT_RATE 500
 #define DEBUGCONSOLE_PROMPT "\n> "
 #define LOG_SAVE
 #define LOG_MAXSIZE 10000
 
+#define WITH_FLOPPY
 #define WITH_SERIAL
 #define WITH_DEBUGCONSOLE
 
