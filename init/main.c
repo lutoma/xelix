b73b39962f0febc92a22f9d5fada51238f20a9f9
Lukas Martini <lukas.martini@unionhost.de>
2010-12-19 13:52:05 +0100

    Log according to standard

--- init/main.c
+++ init/main.c
@@ -46,15 +46,15 @@ static void compilerInfo()
 // Check if ints have the right length
 static void checkIntLenghts()
 {
-	log("Checking length of uint8... ");
+	log("init: Checking length of uint8... ");
 	ASSERT(sizeof(uint8) == 1);
 	log("Right\n");
 	
-	log("Checking length of uint16... ");
+	log("init_ Checking length of uint16... ");
 	ASSERT(sizeof(uint16) == 2);
 	log("Right\n");
 	
-	log("Checking length of uint32... ");
+	log("init: Checking length of uint32... ");
 	ASSERT(sizeof(uint32) == 4);
 	log("Right\n");
 }
