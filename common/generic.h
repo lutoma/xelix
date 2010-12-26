8681c6d5acf7a7aa9be4d104aecd5d28e10265f4
Lukas Martini <lukas.martini@unionhost.de>
2010-12-26 17:55:21 +0100

    Added DUMPVAR for easy variable dumping

--- common/generic.h
+++ common/generic.h
@@ -9,6 +9,7 @@
 
 #define isdigit(C) ((C) >= '0' && (C) <= '9')
 #define IFDEFC(D,C) if(D) C;
+#define DUMPVAR(C,D) log("%%dumpvar: %s="C" at %s:%d%%\n", 0x02, #D, D, __FILE__, __LINE__);
 
 // Typedefs
 typedef unsigned long uint64;
