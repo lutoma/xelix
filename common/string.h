a733215180c49a0e2611ddc7598c60d23d22dfa6
Lukas Martini <lukas.martini@unionhost.de>
2010-12-28 18:41:32 +0100

    Added strtok

--- common/string.h
+++ common/string.h
@@ -4,10 +4,11 @@
 
 
 // STRING FUNCTIONS (similiar to the string.h C standard library)
-
 int strcmp(const char *s1, const char *s2);
 char *strcat(char *dest, const char *src);
 char *strcpy(char *dest, const char *src);
 char *strncpy(char *dst, const char *src, size_t n);
 size_t strlen(const char * str);
 char* substr(char* src, size_t start, size_t len);
+char * strtok(char *s, const char *delim);
+char * strtok_r(char *s, const char *delim, char **last);
