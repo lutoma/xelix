0393fc674905ee446a9cc4a86108404438e89cdf
Lukas Martini <lukas.martini@unionhost.de>
2010-12-29 02:25:18 +0100

    Various improvements in fio.h, fio.c

--- common/fio.c
+++ common/fio.c
@@ -7,32 +7,42 @@
 #include <memory/kmalloc.h>
 #include <common/string.h>
 
+// Caution: Some^W most of those functions are untested, beware of the bugs ;)
+
+// Get Node from Path, return NULL if error
+fsNode_t* fio_pathToNode(const char* __path)
+{
+	// Split path and iterate trough the single parts, going from / upwards.
+	static char* __pch;
+	__pch = strtok(__path, "/");	
+	fsNode_t* __node = vfs_rootNode->finddir(vfs_rootNode, __pch);
+
+	while(__pch != NULL && __node != NULL)
+	{
+		__pch = strtok(NULL, "/");		
+		__node = vfs_rootNode->finddir(__node, __pch);
+	}
+	return __node;
+}
+
 FILE* fopen(const char* __path, const char* __modes)
 {
 	log("fio: Opening file %s, modes '%s'\n", __path, __modes);
 
-	// Split path and iterate trough the single parts, going from / upwards.
-	char* __pch;
-	__pch = strtok(__path,"/");	
-	fsNode_t* __node = vfs_finddirNode(rootNode, __pch);
-
-	while(__pch != NULL)
-	{
-		__pch = strtok(NULL, "/");		
-		__node = vfs_finddirNode(__node, __pch);
-
-		if(__node == NULL)
-			return NULL;
-	}
+	fsNode_t* __node = fio_pathToNode(__path);
+	if(__node == NULL)
+		return;
 
 	// Tell the driver of the device this file is on we want to open it
 	if(__node->open != NULL)
-		__node->open(__node);
+		if(__node->open(__node) == 1)
+			return NULL;
 
 	FILE* fp = kmalloc(sizeof(FILE));
 	fp->path = __path;
 	fp->modes = __modes;
 	fp->node = __node;
+	fp->position = 0;
 	return fp;
 }
 
@@ -47,8 +57,40 @@ int fclose(FILE* __fp)
 	kfree(__fp);
 }
 
+char fgetc(FILE* fp)
+{
+	if(fp->node->read == NULL)
+		return EOF;
+	static char c[1];
+	size_t __size = fp->node->read(fp->node, fp->position, 1, c);
+	fp->position++;
+	return c[0];
+}
+
+int fputc(int c, FILE* fp)
+{
+	if(fp->node->write == NULL)
+		return 1;
+
+	static char __c[1];
+	return fp->node->write(fp->node, fp->position, 1, __c);
+}
 /*
-int fgetc(FILE* fp);
-size_t fwrite (const void *array, size_t size, size_t count, FILE *stream);
-int fputc(int c, FILE *fp);
+size_t fwrite (const void *array, size_t size, size_t count, FILE *fp)
+{
+	if(fp->node->write == NULL)
+		return 1;
+
+	static char __c[1];
+	return fp->node->write(fp->node, fp->position, 1, __c);
+}
 */
+void rewind(FILE* fp)
+{
+	fp->position = 0;
+}
+
+int scandir(const char* dirp, struct dirent*** namelist, int (*filter)(const struct dirent *), int (*compar)(const struct dirent **, const struct dirent **))
+{
+	return 0;
+}
--- common/fio.h
+++ common/fio.h
@@ -9,11 +9,14 @@ typedef struct FILE
 	const char* path;
 	const char* modes;
 	fsNode_t* node;
+	uint32 position;
 } FILE;
 
 FILE *fopen(const char* path, const char* mode);
 //FILE *freopen(const char *path, const char *mode, FILE *fp);
 int fclose(FILE* fp);
-int fgetc(FILE* fp);
+char fgetc(FILE* fp);
 size_t fwrite (const void *array, size_t size, size_t count, FILE *stream);
 int fputc(int c, FILE *fp);
+void rewind(FILE *fp);
+int scandir(const char* dirp, struct dirent*** namelist, int (*filter)(const struct dirent *), int (*compar)(const struct dirent **, const struct dirent **))
