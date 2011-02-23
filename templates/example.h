2e5441c8d7cc7b6825562bbee74d87ab4d1cfeed
Lukas Martini <lukas.martini@unionhost.de>
2011-02-23 13:50:16 +0100

    Remove useless whitespace

--- templates/example.h
+++ templates/example.h
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Xelix.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Xelix. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <common/generic.h>
