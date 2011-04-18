dc2686f42e5e3a4fe3a493c42a4910fad4126eb1
Lukas Martini <lukas.martini@unionhost.de>
2011-04-18 13:54:00 +0200

    scheduler: Adding debugging options, fixing stack pointer.

--- src/tasks/scheduler.c
+++ src/tasks/scheduler.c
@@ -24,6 +24,19 @@
 #include <memory/kmalloc.h>
 #include <hw/cpu.h>
 
+/* You most probably don't want to touch this one. Uncommenting this
+ * line will get you many many debug lines. And by many I mean 'about
+ * 1000 per second'.
+ */
+//#define DEBUG
+#define STACKSIZE 4096
+
+#ifdef DEBUG
+	#define intDebug(args...) log(args);
+#else /* DEBUG */
+	#define intDebug
+#endif /* DEBUG */
+
 task_t* firstTask = NULL;
 task_t* currentTask = NULL;
 task_t* lastTask = NULL;
@@ -41,7 +54,9 @@ void scheduler_add(void* entry)
 	thisTask->state = (cpu_state_t*)kmalloc(sizeof(cpu_state_t));
 	memset(thisTask->state, 0, sizeof(cpu_state_t));
 	
-	thisTask->state->esp = (uint32_t)kmalloc(4096); // Stack
+	// Stack
+	thisTask->state->esp = (uint32_t)kmalloc(STACKSIZE) + STACKSIZE;
+	// Instruction pointer (= start of the program)
 	thisTask->state->eip = (uint32_t)entry;
 	thisTask->state->eflags = 0x200;
 	thisTask->state->cs = 0x08;
@@ -65,6 +80,7 @@ task_t* scheduler_getCurrentTask()
 // Called by the PIT a few hundred times per second.
 task_t* scheduler_select(cpu_state_t* lastRegs)
 {
+		intDebug("scheduler: scheduling...\n");
 		// No task at all
 		if(firstTask == NULL)
 			return (task_t*)NULL;
@@ -72,10 +88,15 @@ task_t* scheduler_select(cpu_state_t* lastRegs)
 		// We have at least one task
 		task_t* nextTask = NULL;
 		
+		intDebug("scheduler: Got at least one task...\n");
+		
 		// Is there a task currently running?
 		if(currentTask != NULL)
 		{
-			//currentTask->state = lastRegs; // Save it's stack.
+			intDebug("scheduler: currentTask at 0x%x\n",
+					(int)currentTask);
+			
+			currentTask->state = lastRegs; // Save it's state.
 			
 			// Look for next task
 			if(currentTask->next != NULL)
@@ -87,5 +108,9 @@ task_t* scheduler_select(cpu_state_t* lastRegs)
 			nextTask = firstTask;
 		
 		currentTask = nextTask;
+		intDebug("scheduler: returning task at 0x%x with state at 0x%x \
+				(esp 0x%x)\n", (int)nextTask, (int)nextTask->state,
+				nextTask->state->esp);
+		
 		return nextTask;
 }
