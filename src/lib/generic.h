ff7de13670a637bc44d1884c3a523acd272f52d9
Lukas Martini <lukas.martini@unionhost.de>
2011-04-18 22:40:53 +0200

    generic.h: Bugfix in init() macro

--- src/lib/generic.h
+++ src/lib/generic.h
@@ -48,12 +48,12 @@ typedef enum { false = 0 , true = 1 } bool;
 #define EOF  -1
 
 // Making ponies fly.
-#define init(C, args...)
-	do
-	{
+#define init(C, args...) \
+	do \
+	{ \
 		log("%%" #C ": Initializing at " __FILE__ ":%d [" #C "_init(" #args ")] (plain)\n%%", 0x03, __LINE__); \
 		C ## _init(args); \
-		log("%%" #C ": Initialized at " __FILE__ ":%d [" #C "_init(" #args ")] (plain)\n%%", 0x03, __LINE__);
+		log("%%" #C ": Initialized at " __FILE__ ":%d [" #C "_init(" #args ")] (plain)\n%%", 0x03, __LINE__); \
 	} while(0);
 
 void outb(uint16_t port, uint8_t value);
