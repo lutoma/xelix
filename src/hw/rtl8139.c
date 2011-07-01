87febdf0ad681566aaa4691613b5ff68cc6c90fb
Lukas Martini <lukas.martini@unionhost.de>
2011-07-02 01:45:11 +0200

    rtl8139: General prettification / Change of the initialization procedure 	modified:   src/hw/rtl8139.c

--- src/hw/rtl8139.c
+++ src/hw/rtl8139.c
@@ -29,6 +29,49 @@
 // This driver will only support that many cards
 #define MAX_CARDS 50 
 
+// Register port numbers
+#define REG_ID0 0x00
+#define REG_ID4 0x04
+ 
+#define REG_TRANSMIT_STATUS0 0x10
+#define REG_TRANSMIT_ADDR0 0x20
+#define REG_RECEIVE_BUFFER 0x30
+#define REG_COMMAND 0x37
+#define REG_CUR_READ_ADDR 0x38
+#define REG_INTERRUPT_MASK 0x3C
+#define REG_INTERRUPT_STATUS 0x3E
+#define REG_TRANSMIT_CONFIGURATION 0x40
+#define REG_RECEIVE_CONFIGURATION 0x44
+ 
+// Control register values
+#define CR_RESET					(1 << 4)
+#define CR_RECEIVER_ENABLE			(1 << 3)
+#define CR_TRANSMITTER_ENABLE		(1 << 2)
+#define CR_BUFFER_IS_EMPTY			(1 << 0)
+ 
+// Transmitter configuration
+#define TCR_IFG_STANDARD			(3 << 24)
+#define TCR_MXDMA_512				(5 << 8)
+#define TCR_MXDMA_1024				(6 << 8)
+#define TCR_MXDMA_2048				(7 << 8)
+ 
+// Receiver configuration
+#define RCR_MXDMA_512				(5 << 8)
+#define RCR_MXDMA_1024				(6 << 8)
+#define RCR_MXDMA_UNLIMITED			(7 << 8)
+#define RCR_ACCEPT_BROADCAST		(1 << 3)
+#define RCR_ACCEPT_MULTICAST		(1 << 2)
+#define RCR_ACCEPT_PHYS_MATCH		(1 << 1)
+ 
+// Interrupt status register
+#define ISR_RECEIVE_BUFFER_OVERFLOW	(1 << 4)
+#define ISR_TRANSMIT_OK				(1 << 2)
+#define ISR_RECEIVE_OK				(1 << 0)
+
+#define int_outb(card, port, value) outb(card ->device->iobase + port, value);
+#define int_outw(card, port, value) outw(card ->device->iobase + port, value);
+#define int_outl(card, port, value) outl(card ->device->iobase + port, value);
+
 struct rtl8139_card {
 	pci_device_t *device;
 	char mac_addr[6];
@@ -39,11 +82,15 @@ static struct rtl8139_card rtl8139_cards[MAX_CARDS];
 
 static void rtl8139_intHandler(cpu_state_t *state)
 {
+	log("rtl8139: Got interrupt \\o/\n");
 }
 
 static void rtl8139_enableCard(struct rtl8139_card *card)
 {
-	/* Load MAC address */
+	// Reset card
+	int_outb(card, REG_COMMAND, CR_RESET);
+
+	// Load MAC address
 	memset(&card->mac_addr, 0, 6);
 	card->mac_addr[0] = inb(card->device->iobase);
 	card->mac_addr[1] = inb(card->device->iobase + 1);
@@ -52,21 +99,21 @@ static void rtl8139_enableCard(struct rtl8139_card *card)
 	card->mac_addr[4] = inb(card->device->iobase + 4);
 	card->mac_addr[5] = inb(card->device->iobase + 5);
 
+	// Enable receiver and transmitter
+	int_outb(card, REG_COMMAND, CR_RECEIVER_ENABLE & CR_TRANSMITTER_ENABLE);
+	int_outl(card, REG_RECEIVE_CONFIGURATION, 0x0000070a);
+	int_outl(card, REG_TRANSMIT_CONFIGURATION, 0x03000700);
+
 	interrupts_registerHandler(card->device->interrupt_line, rtl8139_intHandler);
 
-	outb(card->device->iobase + 0x37, 2 << 3);
-	while ((inb(card->device->iobase + 0x37) & 16) != 0)
-		printf("V");
-	outb(card->device->iobase + 0x37, 2 << 2 | 2 << 1);
-
-	outl(card->device->iobase + 0x40, 0x03000700);
-	outl(card->device->iobase + 0x44, 0x0000070a);
-
+	// Set receive buffer
 	card->rx_buffer = (char *)kmalloc(8192 + 16);
-	outl(card->device->iobase + 0x30, (uint32_t)card->rx_buffer);
+	int_outl(card, REG_RECEIVE_BUFFER, (uint32_t)card->rx_buffer);
 
-	outw(card->device->iobase + 0x3e, 0);
-	outw(card->device->iobase + 0x3c, 0xffff);
+	int_outw(card, REG_INTERRUPT_STATUS, 0);
+	
+	// Enable all interrupt events
+	int_outw(card, REG_INTERRUPT_MASK, 0xffff);
 }
 
 void rtl8139_init()
