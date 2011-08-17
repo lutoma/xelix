9ad92e6a0bf2d85e8855b04ea6464c67e53ca592
Fritz Grimpen <fritz@grimpen.net>
2011-08-17 14:56:20 +0200

    Implemented simple write syscall

--- src/tasks/syscall.c
+++ src/tasks/syscall.c
@@ -23,6 +23,8 @@
 #include <lib/log.h>
 #include <lib/print.h>
 #include <tasks/scheduler.h>
+#include <lib/string.h>
+#include <console/interface.h>
 
 typedef int (*syscall_t)(cpu_state_t *);
 
@@ -32,9 +34,22 @@ static int syscall_exit(cpu_state_t *regs)
 	return 0;
 }
 
-static int syscall_print(cpu_state_t *regs)
+static int syscall_write(cpu_state_t *regs)
 {
-	return print((char *)regs->ecx);
+	if (regs->ebx == 1 || regs->ebx == 2)
+	{
+		if (regs->ebx == 2)
+			console_write2(NULL, "\e[31m");
+
+		int retval = console_write(NULL, (char*)regs->ecx, regs->edx);
+
+		if (regs->ebx == 2)
+			console_write2(NULL, "\e[39m");
+
+		return retval;
+	}
+
+	return -1;
 }
 
 static int syscall_getpid(cpu_state_t *regs)
@@ -47,11 +62,19 @@ static int syscall_getppid(cpu_state_t *regs)
 	return scheduler_getCurrentTask()->parent;
 }
 
+static int syscall_print(cpu_state_t *regs)
+{
+	return console_write2(NULL, (char*)regs->ebx);
+}
+
 static syscall_t jump_table[] = {
-	syscall_print,   /* 0 */
-	syscall_exit,    /* 1 */
-	syscall_getpid,  /* 2 */
-	syscall_getppid, /* 3 */
+	syscall_print, // 0
+  syscall_exit, // 1
+	NULL,
+	NULL,
+	syscall_write, // 4
+	syscall_getppid,
+	syscall_getpid
 };
 
 static void intHandler(cpu_state_t* regs)
