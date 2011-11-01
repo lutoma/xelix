9f35937b1780f96c9ca130f9005e63011d9884b3
Lukas Martini <lukas.martini@unionhost.de>
2011-11-01 17:10:14 +0100

    Fixing license information

--- templates/example.c
+++ templates/example.c
@@ -1,19 +1,18 @@
 /* Copyright © <year> <your name>
  *
- * This file is part of Xelix.
+ * This file is part of Xlibc.
  *
- * Xelix is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Xlibc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * Xelix is distributed in the hope that it will be useful,
+ * Xlibc is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Xelix. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Xlibc. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Your code
