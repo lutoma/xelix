73a2aeb9cdef82d2c6b4031d3de81eb85e986915
Fritz Grimpen <fritz@grimpen.net>
2011-11-15 17:06:29 +0100

    paging: Fixed Bug #30, which prevented Thinkpad X Series from booting Xelix without triplefault

--- src/memory/paging.c
+++ src/memory/paging.c
@@ -110,6 +110,12 @@ int paging_apply(struct vm_context *ctx)
 	{
 		/* Build paging context for vm_context */
 		pgCtx = kmalloc_a(sizeof(struct paging_context));
+		if (pgCtx == NULL)
+		{
+			interrupts_enable();
+			return false;
+		}
+		memset(pgCtx, 0, sizeof(struct paging_context));
 		vm_set_cache(ctx, pgCtx);
 		
 		vm_iterate(ctx, paging_vmIterator);
