7dc7d853b9d040a6d787df3724ab286cd502da38
Lukas Martini <lukas.martini@unionhost.de>
2011-11-17 15:40:21 +0100

    string: Adding strtok_r and strtok

--- include/string.h
+++ include/string.h
@@ -23,3 +23,5 @@
 void memcpy(void* dest, void* src, uint32_t size);
 void memset(void* ptr, uint8_t fill, uint32_t size);
 size_t strlen(const char* str);
+char* strtok(char* str, const char* delim);
+char* strtok_r(char* s, const char* delim, char** last);
