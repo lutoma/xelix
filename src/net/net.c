3f82c3d6841e661251a0473ceb847afa5e171608
Lukas Martini <lutoma@ohai.su>
2011-11-27 10:11:01 +0100

    net: Adding net.[ch] barebones

---
+++ src/net/net.c
@@ -0,0 +1,37 @@
+/* net.c: Network organization
+ * Copyright © 2011 Lukas Martini
+ *
+ * This file is part of Xelix.
+ *
+ * Xelix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Xelix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Xelix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "net.h"
+
+#include <lib/log.h>
+#include <tasks/scheduler.h>
+
+#define MAX_DEVICES 51
+net_device_t registered_devices[MAX_DEVICES];
+uint32_t registered_device_count;
+
+void net_incoming(net_device_t* origin, int target, void* data)
+{
+	log(LOG_DEBUG, "net: net_incoming: Incoming packet from %s with target %d.\n", origin->name, target);
+}
+
+void net_register_device(net_device_t* device)
+{
+	registered_devices[registered_device_count++] = device;
+}
