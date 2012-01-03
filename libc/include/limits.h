7ca76fb302e810b38416f41ba1cb50b84d3375bd
Lukas Martini <lutoma@ohai.su>
2012-01-03 08:46:40 +0100

    libc: limits.h: Adding numerical limits

--- libc/include/limits.h
+++ libc/include/limits.h
@@ -17,7 +17,29 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with Xlibc. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
+#define CHAR_BIT 8
+#define CHAR_MIN 0
+#define INT_MAX 2147483647
+#define LONG_BIT 32
+#define LONG_MAX 2147483647
+#define MB_LEN_MAX 1
+#define SCHAR_MAX 1
+#define CHAR_MAX SCHAR_MAX
+#define SHRT_MAX 32767
+#define UCHAR_MAX 255
+#define UINT_MAX 4294967295
+#define ULONG_MAX 4294967295
+#define SSIZE_MAX UINT_MAX
+#define USHRT_MAX 65535
+#define WORD_BIT 3
+#define INT_MIN -2147483647
+#define LONG_MIN -2147483647
+#define SCHAR_MIN -128
+#define SHRT_MIN -32767
+#define LLONG_MIN -9223372036854775807
+#define LLONG_MAX 9223372036854775807
+#define ULLONG_MAX 18446744073709551615
 
 #define _POSIX_PATH_MAX 256
 #define PATH_MAX 512
