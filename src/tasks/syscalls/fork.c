6fce1b205f73ebf187d2a855d31f49643895b075
Lukas Martini <hello@lutoma.org>
2014-12-01 17:07:28 +0100

    Adding fork syscall boilerplate (always returns error).

---
+++ src/tasks/syscalls/fork.c
@@ -0,0 +1,28 @@
+/* fork.c: Fork Syscall
+ * Copyright © 2014 Lukas Martini
+ *
+ * This file is part of Xelix.
+ *
+ * Xelix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Xelix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Xelix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <lib/log.h>
+#include <tasks/syscall.h>
+
+int sys_fork(struct syscall syscall)
+{
+	log(LOG_INFO, "sys_fork: Received fork syscall. Discarding and returning -1.\n");
+	return -1;
+}
+	
