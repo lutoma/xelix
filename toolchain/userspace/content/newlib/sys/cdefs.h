a683a22f28172538f62cafbee04d15391b666e1a
Lukas Martini <hello@lutoma.org>
2015-05-22 07:16:43 +0200

    userspace sys/cdefs.h: Remove double ifndef check for __GNUC_PREREQ

--- toolchain/userspace/content/newlib/sys/cdefs.h
+++ toolchain/userspace/content/newlib/sys/cdefs.h
@@ -55,8 +55,6 @@
 
 // Xelix: Quite hacky to include this here
 #ifndef __GNUC_PREREQ
-
-#ifndef __GNUC_PREREQ
 # if defined __GNUC__ && defined __GNUC_MINOR__
 #  define __GNUC_PREREQ(maj, min) \
   ((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
@@ -64,8 +62,6 @@
 #  define __GNUC_PREREQ(maj, min) 0
 # endif
 #endif /* __GNUC_PREREQ */
-
-#endif
 
 /* Some user header file might have defined this before.  */
 #undef	__P
