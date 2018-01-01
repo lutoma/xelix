3c6382e6c9ab2e145ec8bd5807107331e9b45353
Lukas Martini <hello@lutoma.org>
2018-01-01 03:00:07 +0100

    vmem: Improve vmem_get_name

--- src/memory/vmem.c
+++ src/memory/vmem.c
@@ -274,6 +274,9 @@ uint32_t vmem_count_pages(struct vmem_context *ctx)
 
 char* vmem_get_name(struct vmem_context* ctx) {
 	if(ctx->task == NULL) {
+		return "Unknown context";
+	}
+	if(ctx->task == vmem_kernelContext) {
 		return "Kernel context";
 	}
 	return ctx->task->name;
