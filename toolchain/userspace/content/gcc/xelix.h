9aec6b941794c55b447f281bc2ecd96f6d4ea051
Lukas Martini <hello@lutoma.org>
2018-07-19 01:16:17 +0200

    Update userland GCC to 8.1.0

--- toolchain/userspace/content/gcc/xelix.h
+++ toolchain/userspace/content/gcc/xelix.h
@@ -3,16 +3,44 @@
     do { \
         builtin_define_std("xelix"); \
         builtin_define_std("unix"); \
+        builtin_define("__xelix__"); \
+        builtin_define("__unix__"); \
         builtin_assert("system=xelix"); \
         builtin_assert("system=unix"); \
+        builtin_assert("system=posix"); \
     } while (0);
 
-#undef TARGET_VERSION
-#define TARGET_VERSION fprintf(stderr, " (i386 xelix)");
+/* Files that are linked before user code.
+   The %s tells GCC to look for these files in the library directory. */
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+
+/* Files that are linked after user code. */
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+
+/* Default arguments you want when running your
+   i686-myos-gcc/x86_64-myos-gcc toolchain */
+#undef LIB_SPEC
+#define LIB_SPEC "-lc" /* link against C standard library */
+
+/* Files that are linked before user code.
+   The %s tells GCC to look for these files in the library directory. */
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+
+/* Files that are linked after user code. */
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+
+/* Don't automatically add extern "C" { } around header files. */
+#undef  NO_IMPLICIT_EXTERN_C
+#define NO_IMPLICIT_EXTERN_C 1
 
 #undef  OBJECT_FORMAT_ELF
 #define OBJECT_FORMAT_ELF
 
 /* This macro applies on top of OBJECT_FORMAT_ELF and indicates that
    we want to support both flat and ELF output.  */
+#undef OBJECT_FORMAT_FLAT
 #define OBJECT_FORMAT_FLAT
