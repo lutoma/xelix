163a2499a721268edcc4771ccaace4b794b91613
Lukas Martini <hello@lutoma.org>
2018-07-22 14:11:53 +0200

    xinit: Don't auto-restart shell

--- land/xinit.c
+++ land/xinit.c
@@ -6,35 +6,35 @@
 #include <sys/execnew.h>
 
 int main() {
-	printf("\nxelix tty1\n\n");
-	printf("localhost login: ");
-	fflush(stdout);
-
-	char* user = malloc(50);
-	char* read = fgets(user, 50, stdin);
-	if(!read) {
-		return 1;
-	}
-
-	printf("Password: ");
-	fflush(stdout);
-
-	char* pass = malloc(500);
-	read = fgets(pass, 500, stdin);
-	if(!read) {
-		return 1;
-	}
-
-	// Strip the \n
-	user[strlen(user) - 1] = (char)0;
-	pass[strlen(pass) - 1] = (char)0;
-
-	printf("\n");
-
-	char* __argv[] = { "xshell", "-l", NULL };
-	char* __env[] = { "PS1=[$USER@$HOST $PWD]# ", "HOME=/root", "TERM=dash", "PWD=/", "USER=root", "HOST=default", NULL }; 
-
 	while(true) {
+		printf("\nxelix tty1\n\n");
+		printf("localhost login: ");
+		fflush(stdout);
+
+		char* user = malloc(50);
+		char* read = fgets(user, 50, stdin);
+		if(!read) {
+			return 1;
+		}
+
+		printf("Password: ");
+		fflush(stdout);
+
+		char* pass = malloc(500);
+		read = fgets(pass, 500, stdin);
+		if(!read) {
+			return 1;
+		}
+
+		// Strip the \n
+		user[strlen(user) - 1] = (char)0;
+		pass[strlen(pass) - 1] = (char)0;
+
+		printf("\n");
+
+		char* __argv[] = { "xshell", "-l", NULL };
+		char* __env[] = { "PS1=[$USER@$HOST $PWD]# ", "HOME=/root", "TERM=dash", "PWD=/", "USER=root", "HOST=default", NULL }; 
+
 		pid_t shell = execnew("/xshell", __argv, __env);
 		if(shell < 1) {
 			printf("xinit: Could not launch shell. Exiting.\n");
@@ -42,6 +42,5 @@ int main() {
 		}
 
 		wait(NULL);
-		printf("xinit: Shell seems to have died, respawning it.\n");
 	}
 }
