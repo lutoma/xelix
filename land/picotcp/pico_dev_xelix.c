2d9404dd6685b04a170b08a5205f9cbe93a7c0aa
Lukas Martini <hello@lutoma.org>
2018-10-02 12:48:03 +0200

    picotcp port improvements

--- land/picotcp/pico_dev_xelix.c
+++ land/picotcp/pico_dev_xelix.c
@@ -1,52 +1,59 @@
 #include "pico_device.h"
 #include "pico_dev_xelix.h"
 #include "pico_stack.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-struct pico_device_null {
-    struct pico_device dev;
-    int statistics_frames_out;
+struct pico_device_xelix {
+	struct pico_device dev;
+	int statistics_frames_out;
 };
 
-#define NULL_MTU 0
+#define XELIX_MTU 1000
+static int devno = 0;
 
-static int pico_xelix_send(struct pico_device *dev, void *buf, int len)
-{
-    struct pico_device_null *null = (struct pico_device_null *) dev;
-    IGNORE_PARAMETER(buf);
-
-    /* Increase the statistic count */
-    null->statistics_frames_out++;
-
-    /* Discard the frame content silently. */
-    return len;
+static int pico_xelix_send(struct pico_device *dev, void *buf, int len) {
+	struct pico_device_xelix* xdev = (struct pico_device_xelix*)dev;
+	write(devno, buf, len);
+	xdev->statistics_frames_out++;
+	return len;
 }
 
-static int pico_xelix_poll(struct pico_device *dev, int loop_score)
-{
-    /* We never have packet to receive, no score is used. */
-    IGNORE_PARAMETER(dev);
-    return loop_score;
+static int pico_xelix_poll(struct pico_device *dev, int loop_score) {
+	/* We never have packet to receive, no score is used. */
+	IGNORE_PARAMETER(dev);
+	return loop_score;
 }
 
-/* Public interface: create/destroy. */
+struct pico_device* pico_xelix_create(const char* name) {
+	struct pico_device_xelix* xdev = PICO_ZALLOC(sizeof(struct pico_device_xelix));
 
+	if(!xdev) {
+		return NULL;
+	}
 
-struct pico_device *pico_xelix_create(const char *name)
-{
-    struct pico_device_null *null = PICO_ZALLOC(sizeof(struct pico_device_null));
+	devno = open("/dev/rtl1", O_RDWR);
+	if(devno == -1) {
+		perror("Could not open /dev/rtl1");
+		return NULL;
+	}
 
-    if (!null)
-        return NULL;
+	dbg("pico_dev_xelix: Opened /dev/rtl1, devno %d\n", devno);
 
-    if( 0 != pico_device_init((struct pico_device *)null, name, NULL)) {
-        return NULL;
-    }
+	if(pico_device_init((struct pico_device*)xdev, name, NULL)) {
+		return NULL;
+	}
 
-    null->dev.overhead = 0;
-    null->statistics_frames_out = 0;
-    null->dev.send = pico_xelix_send;
-    null->dev.poll = pico_xelix_poll;
-    dbg("Device %s created.\n", null->dev.name);
-    return (struct pico_device *)null;
+	xdev->dev.overhead = 0;
+	xdev->statistics_frames_out = 0;
+	xdev->dev.send = pico_xelix_send;
+	xdev->dev.poll = pico_xelix_poll;
+	return (struct pico_device*)xdev;
 }
 
+static void __attribute__((destructor)) _close_xelix_dev(void) {
+	if(devno) {
+		close(devno);
+	}
+}
--- land/picotcp/pico_xelix.h
+++ land/picotcp/pico_xelix.h
@@ -16,23 +16,30 @@
 #define pico_free(x) free(x)
 
 static inline uint32_t PICO_TIME(void) {
-    struct timeval t;
-    gettimeofday(&t, NULL);
-    return (uint32_t)t.tv_sec;
+	struct timeval t;
+	if(gettimeofday(&t, NULL) == -1) {
+		perror("Could not get time");
+		exit(EXIT_FAILURE);
+	}
+	return (uint32_t)t.tv_sec;
 }
 
 static inline uint64_t PICO_TIME_MS(void) {
-    struct timeval t;
-    uint64_t time;
-    gettimeofday(&t, NULL);
+	struct timeval t;
+	uint64_t time;
 
-    time = (uint64_t)((t.tv_sec * 1000) + (t.tv_usec / 1000));
-    return time;
+	if(gettimeofday(&t, NULL) == -1) {
+		perror("Could not get time");
+		exit(EXIT_FAILURE);
+	}
+
+	time = (uint64_t)((t.tv_sec * 1000) + (t.tv_usec / 1000));
+	return time;
 }
 
 static inline void PICO_IDLE(void) {
-    printf("sleep stub.\n");
-    //usleep(5000);
+	printf("sleep stub.\n");
+	//usleep(5000);
 }
 
 #endif  /* PICO_SUPPORT_XELIX */
