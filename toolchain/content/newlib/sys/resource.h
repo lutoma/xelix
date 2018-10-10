4d2977e4592304dc0ed069027ed5a486f0758fc9
Lukas Martini <hello@lutoma.org>
2018-10-10 20:42:53 +0200

    toolchain: Add extended sys/resource.h

---
+++ toolchain/content/newlib/sys/resource.h
@@ -0,0 +1,36 @@
+#ifndef _SYS_RESOURCE_H_
+#define _SYS_RESOURCE_H_
+
+#include <sys/time.h>
+
+#define	RUSAGE_SELF	0		/* calling process */
+#define	RUSAGE_CHILDREN	-1		/* terminated child processes */
+
+#define RLIM_INFINITY 0
+#define RLIMIT_CORE 1
+#define RLIMIT_CPU 2
+#define RLIMIT_DATA 3
+#define RLIMIT_FSIZE 4
+#define RLIMIT_NOFILE 5
+#define RLIMIT_STACK 6
+#define RLIMIT_AS 7
+
+struct rusage {
+  	struct timeval ru_utime;	/* user time used */
+	struct timeval ru_stime;	/* system time used */
+};
+
+typedef uint32_t rlim_t;
+
+struct rlimit {
+	rlim_t rlim_cur;  /* Soft limit */
+	rlim_t rlim_max;  /* Hard limit (ceiling for rlim_cur) */
+};
+
+int  getpriority(int, id_t);
+int  getrlimit(int, struct rlimit *);
+int  getrusage(int, struct rusage *);
+int  setpriority(int, id_t, int);
+int  setrlimit(int, const struct rlimit *);
+#endif
+
