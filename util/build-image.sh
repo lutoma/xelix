ef41506f63d6950b16a6f6a137fcf926b82ecc8b
Lukas Martini <hello@lutoma.org>
2019-01-26 14:37:08 +0100

    util/build-image.sh: Install GRUB2 to image

--- util/build-image.sh
+++ util/build-image.sh
@@ -13,14 +13,31 @@ sudo modprobe nbd max_part=8
 
 if [ -f "$dest" ]; then
 	sudo qemu-nbd --connect=/dev/nbd0 "$dest"
+	sudo mount /dev/nbd0p1 mnt
 else
 	qemu-img create -f qcow2 "$dest" 300M
 	sudo qemu-nbd --connect=/dev/nbd0 "$dest"
+
 	echo "/dev/nbd0p1 : start=2048, type=83" | sudo sfdisk /dev/nbd0
 	sudo mkfs.ext2 /dev/nbd0p1
+	sudo mount /dev/nbd0p1 mnt
+
+	sudo grub-install /dev/nbd0 --boot-directory=mnt/boot --modules="normal part_msdos ext2 multiboot" --no-floppy --target=i386-pc
+	cat <<EOF | sudo sponge mnt/boot/grub/grub.cfg
+set root='hd0,msdos1'
+set timeout=1
+
+menuentry 'Xelix' {
+	set gfxmode=720x480x24
+	set gfxpayload=keep
+	insmod vbe
+	insmod gfxterm
+	multiboot2 "(\$root)/boot/xelix.bin"
+}
+EOF
+
 fi
 
-sudo mount /dev/nbd0p1 mnt
 
 for i in land/*; do
 	echo "Building $i"
