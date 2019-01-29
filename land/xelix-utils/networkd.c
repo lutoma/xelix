48282eeae6dfad216ebda3ff8ddfa0329c363034
Lukas Martini <hello@lutoma.org>
2019-01-29 19:29:22 +0100

    networkd improvements

--- land/xelix-utils/networkd.c
+++ land/xelix-utils/networkd.c
@@ -1,5 +1,6 @@
 #include <time.h>
 #include <sched.h>
+#include <stdbool.h>
 #include <pico_stack.h>
 #include <pico_ipv4.h>
 #include <pico_icmp4.h>
@@ -9,9 +10,63 @@
 #include <pico_ipv4.h>
 
 #define BSIZE (1024 * 10)
+uint32_t dhcpclient_xid;
 static char recvbuf[BSIZE];
-static int pos = 0, len = 0;
-static int flag = 0;
+
+
+void telnet_cb(uint16_t ev, struct pico_socket *s) {
+	if (ev & PICO_SOCK_EV_CONN) {
+		struct pico_ip4 orig = {0};
+		uint16_t port = 0;
+		struct pico_socket *sock_a = pico_socket_accept(s, &orig, &port);
+
+		char peer[30] = {0};
+		pico_ipv4_to_string(peer, orig.addr);
+		printf("Telnet connection from %s:%d.\n", peer, short_be(port));
+	}
+
+	if (ev & PICO_SOCK_EV_RD) {
+		int r = pico_socket_read(s, recvbuf, BSIZE);
+		recvbuf[r - 1] = 0;
+		printf("Received %s\n", recvbuf);
+
+		size_t buf_size = r >= 150 ? r : 150;
+		char* buf = calloc(buf_size, 1);
+		memcpy(buf, recvbuf, r);
+
+		if(pico_socket_write(s, (const uint8_t *)buf, buf_size) == -1) {
+			fprintf(stderr, "Could not write.\n");
+		}
+
+		free(buf);
+	}
+}
+
+void callback_dhcpclient(void* cli, int code) {
+	if(code & PICO_DHCP_ERROR) {
+		printf("DHCP failed.\n");
+		return;
+	}
+
+	struct pico_ip4 ipaddr = pico_dhcp_get_address(cli);
+	char ip[16];
+	pico_ipv4_to_string(ip, ipaddr.addr);
+	printf("DHCP done, IP %s\n", ip);
+
+	struct pico_socket* s = pico_socket_open(PICO_PROTO_IPV4, PICO_PROTO_TCP, &telnet_cb);
+	if(!s) {
+		fprintf(stderr, "Error opening.\n");
+	}
+
+	uint16_t listen_port = short_be(23);
+	if(pico_socket_bind(s, &ipaddr.addr, &listen_port) < 0) {
+		fprintf(stderr, "Error binding.\n");
+	}
+
+	if(pico_socket_listen(s, 40) < 0) {
+		fprintf(stderr, "Error listening.\n");
+	}
+}
 
 int main(void){
 	pico_stack_init();
@@ -20,23 +75,9 @@ int main(void){
 		return 1;
 	}
 
-	struct pico_ip4 ipaddr, netmask, gateway, route;
-	pico_string_to_ipv4("10.111.1.42", &ipaddr.addr);
-	pico_string_to_ipv4("255.255.255.0", &netmask.addr);
-	pico_string_to_ipv4("10.111.1.1", &gateway.addr);
-	pico_string_to_ipv4("0.0.0.0", &route.addr);
+	pico_dhcp_initiate_negotiation(dev, &callback_dhcpclient, &dhcpclient_xid);
 
-	pico_ipv4_link_add(dev, ipaddr, netmask);
-
-	struct pico_ipv4_link* link = pico_ipv4_link_get(&ipaddr);
-	pico_ipv4_route_add(route, route, gateway, 100, link);
-
-	//pico_dhcp_initiate_negotiation(dev, &callback_dhcpclient, &dhcpclient_xid);
-	//pico_icmp4_ping("172.22.119.4", NUM_PING, 500, 10000, 64, cb_ping);
-
-	printf("Launched PicoTCP.\n");
-
-	while (1) {
+	while(true) {
 		pico_stack_tick();
 	}
 
