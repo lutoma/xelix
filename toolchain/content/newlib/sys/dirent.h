3016dc2a8fdeee6507c017d9aa4a2464e5020f3b
Lukas Martini <hello@lutoma.org>
2019-02-22 00:15:03 +0100

    toolchain: Adjust dirent file type numbers to those used in ext2

--- toolchain/content/newlib/sys/dirent.h
+++ toolchain/content/newlib/sys/dirent.h
@@ -12,28 +12,27 @@
 #include <sys/lock.h>
 #undef _LIBC
 
-#define HAVE_NO_D_NAMLEN	/* no struct dirent->d_namlen */
-#define HAVE_DD_LOCK  		/* have locking mechanism */
+#define HAVE_NO_D_NAMLEN    /* no struct dirent->d_namlen */
+#define HAVE_DD_LOCK        /* have locking mechanism */
 
-#define MAXNAMLEN 255		/* sizeof(struct dirent.d_name)-1 */
+#define MAXNAMLEN 255       /* sizeof(struct dirent.d_name)-1 */
 
-/* xelix - these should be linux specific, but nano wants them. */
-#define DT_BLK 1
-#define DT_CHR 2
-#define DT_DIR 3
-#define DT_FIFO 4
-#define DT_LNK 5
-#define DT_REG 6
-#define DT_SOCK 7
 #define DT_UNKNOWN 0
+#define DT_REG 1
+#define DT_DIR 2
+#define DT_CHR 3
+#define DT_BLK 4
+#define DT_FIFO 5
+#define DT_SOCK 6
+#define DT_LNK 7
 
 typedef struct {
-    int dd_fd;		/* directory file */
-    int dd_loc;		/* position in buffer */
+    int dd_fd;      /* directory file */
+    int dd_loc;     /* position in buffer */
     int dd_seek;
-    char *dd_buf;	/* buffer */
-    int dd_len;		/* buffer length */
-    int dd_size;	/* amount of data in buffer */
+    char *dd_buf;   /* buffer */
+    int dd_len;     /* buffer length */
+    int dd_size;    /* amount of data in buffer */
     _LOCK_RECURSIVE_T dd_lock;
 } DIR;
 
