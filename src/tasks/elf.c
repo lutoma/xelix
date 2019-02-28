50a591375c86f807d9d9c8595412b9fb2df62d34
Lukas Martini <hello@lutoma.org>
2019-02-28 05:57:32 +0100

    elf: Loading improvements, hacky interpreter support

--- src/tasks/elf.c
+++ src/tasks/elf.c
@@ -57,6 +57,17 @@
 #define SHT_LOUSER 0x80000000
 #define SHT_HIUSER 0xffffffff
 
+#define PT_NULL 0
+#define PT_LOAD 1
+#define PT_DYNAMIC 2
+#define PT_INTERP 3
+#define PT_NOTE 4
+#define PT_SHLIB 5
+#define PT_PPHEAD 6
+#define PT_HISUNW 0x6fffffff
+#define PT_LOPROC 0x70000000
+#define PT_HIPROC 0x7fffffff
+
 #define ELF_TYPE_NONE 0
 #define ELF_TYPE_REL 1
 #define ELF_TYPE_EXEC 2
@@ -70,90 +81,87 @@
 
 static char elf_magic[16] = {0x7f, 'E', 'L', 'F', 01, 01, 01, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-static int read_sections(int fd, elf_t* header, void* binary_start, uint32_t memsize, task_t* task) {
-	elf_section_t* section_headers = kmalloc(header->shnum * header->shentsize);
-	vfs_seek(fd, header->shoff, VFS_SEEK_SET, task->parent);
+static inline void* bin_read(int fd, task_t* task, size_t offset, size_t size, void* inbuf) {
+	void* buf = inbuf ? inbuf : kmalloc(size);
+	vfs_seek(fd, offset, VFS_SEEK_SET, task->parent);
+	size_t read = vfs_read(fd, buf, size, task->parent);
+	if(likely(read == size)) {
+		return buf;
+	}
+	if(!inbuf) {
+		kfree(buf);
+	}
+	return NULL;
+}
 
-	size_t read = vfs_read(fd, section_headers, header->shnum * header->shentsize, task->parent);
-	if(read != header->shnum * header->shentsize) {
-		kfree(section_headers);
+static int read_sections(int fd, elf_t* header, void* binary_start, uint32_t memsize, task_t* task) {
+	elf_section_t* shead_start = bin_read(fd, task, header->shoff,
+		header->shnum * header->shentsize, NULL);
+	if(!shead_start) {
 		return -1;
 	}
 
-	elf_section_t* shead = section_headers;
+	elf_section_t* shead = shead_start;
 	int loaded_sections = 0;
 	for(int i = 0; i < header->shnum; i++) {
-
-		/* Sections without a type are unused/empty. This is usually the case
-		 * for the first section.
-		 */
-		if(!shead->type) {
-			goto section_cont;
-		}
-
 		debug("elf: Section %d, type 0x%x, size 0x%x, offset 0x%x\n",
 			i, shead->type, shead->size, shead->offset);
 
-		// We only care about sections we have to load here
-		if(shead->type == SHT_NOBITS || !(shead->flags & SHF_ALLOC)) {
+		/* Sections without a type are unused/empty. This is usually the case
+		 * for the first section. We also only care about loadable sections.
+		 */
+		if(!shead->type || shead->type == SHT_NOBITS || !(shead->flags & SHF_ALLOC)) {
 			goto section_cont;
 		}
 
 		void* dest = (void*)vmem_translate(task->memory_context, (intptr_t)shead->addr, false);
-		if(!dest) {
-			kfree(section_headers);
-			return -1;
-		}
+		if(unlikely(!dest || shead->offset + shead->size > memsize ||
+			!bin_read(fd, task, shead->offset, shead->size, dest))) {
 
-		debug("elf: - Physical addr. 0x%x - 0x%x, virtual 0x%x - 0x%x\n",
-			dest, dest + shead->size,
-			shead->addr, shead->addr + shead->size);
-
-		if(shead->offset + shead->size > memsize) {
-			kfree(section_headers);
-			return -1;
-		}
-
-		vfs_seek(fd, shead->offset, VFS_SEEK_SET, task->parent);
-		size_t read = vfs_read(fd, dest, shead->size, task->parent);
-		if(read != shead->size) {
-			kfree(section_headers);
+			kfree(shead_start);
 			return -1;
 		}
 
 		loaded_sections++;
+		debug("elf: Mapped from phys 0x%x - 0x%x to virt 0x%x - 0x%x\n",
+			dest, dest + shead->size, shead->addr, shead->addr + shead->size);
 
 		section_cont:
 		shead = (elf_section_t*)((intptr_t)shead + (intptr_t)header->shentsize);
 	}
 
-	kfree(section_headers);
+	kfree(shead_start);
 	return loaded_sections;
 }
 
-static uint32_t alloc_memory(int fd, elf_t* header, void** binary_start, task_t* task) {
+static uint32_t read_pheads(int fd, elf_t* header, void** binary_start, char** interp, task_t* task) {
 	/* Allocates a physical memory region for this binary based on the memory
 	 * requirements in the program headers. The binary will then get copied
 	 * to that region based on the sections.
 	 */
 
-	void* program_headers = kmalloc(header->phnum * header->phentsize);
-	vfs_seek(fd, header->phoff, VFS_SEEK_SET, task->parent);
-	size_t read = vfs_read(fd, program_headers, header->phnum * header->phentsize, task->parent);
-	if(read != header->phnum * header->phentsize) {
-		kfree(program_headers);
+	elf_program_header_t* phead_start = bin_read(fd, task, header->phoff,
+		header->phnum * header->phentsize, NULL);
+	if(unlikely(!phead_start)) {
 		return -1;
 	}
 
 	uint32_t memsize = 0;
-	intptr_t phead = (intptr_t)program_headers;
-	void* pages_start = ((elf_program_header_t*)phead)->vaddr;
+	elf_program_header_t* phead = phead_start;
+	void* pages_start = phead->vaddr;
 
 	for(int i = 0; i < header->phnum; i++) {
-		memsize += ((elf_program_header_t*)phead)->memsz;
-		phead = phead + header->phentsize;
+		switch(phead->type) {
+			case PT_LOAD:
+				memsize += phead->memsz; break;
+			case PT_INTERP:;
+				*interp = bin_read(fd, task, phead->offset, phead->filesz, NULL);
+				return 0;
+		}
+
+		phead = (elf_program_header_t*)((intptr_t)phead + header->phentsize);
 	}
-	kfree(program_headers);
+	kfree(phead_start);
 
 	memsize = VMEM_ALIGN(memsize) + PAGE_SIZE;
 	task->sbrk = pages_start + memsize;
@@ -180,13 +188,12 @@ if(unlikely(!(cmp))) {                         \
 int elf_load_file(task_t* task, char* path) {
 	// Need to use task->parent to resolve relative paths from excecve
 	int fd = vfs_open(path, O_RDONLY, task->parent);
-	if(fd == -1) {
+	if(unlikely(fd == -1)) {
 		return -1;
 	}
 
-	elf_t* header = kmalloc(sizeof(elf_t));
-	size_t read = vfs_read(fd, (void*)header, sizeof(elf_t), task->parent);
-	LF_ASSERT(read == sizeof(elf_t), "Could not read ELF header");
+	elf_t* header = bin_read(fd, task, 0, sizeof(elf_t), NULL);
+	LF_ASSERT(header, "Could not read ELF header");
 	LF_ASSERT(!memcmp(header->ident, elf_magic, sizeof(elf_magic)),
 		"Invalid magic");
 
@@ -199,8 +206,18 @@ int elf_load_file(task_t* task, char* path) {
 	task_set_initial_state(task, header->entry);
 
 	void* binary_start = NULL;
-	uint32_t memsize = alloc_memory(fd, header, &binary_start, task);
-	LF_ASSERT(memsize > 0 && binary_start, "Loading program headers failed.");
+	char* interp = NULL;
+	uint32_t memsize = read_pheads(fd, header, &binary_start, &interp, task);
+	LF_ASSERT((memsize > 0 && binary_start) || interp, "Loading program headers failed.");
+
+	// If we the binary has an interpreter/linker set, run it.
+	if(interp) {
+		kfree(header);
+		vfs_close(fd, task->parent);
+		int r = elf_load_file(task, interp);
+		kfree(interp);
+		return r;
+	}
 
 	int loaded = read_sections(fd, header, binary_start, memsize, task);
 	LF_ASSERT(loaded > 0, "Loading sections failed.");
