5760ac3baf1595146a80d346f0fa318283e5d0f0
Lukas Martini <hello@lutoma.org>
2019-05-13 09:56:48 +0200

    tty: ARM uart improvements

--- src/tty/serial.c
+++ src/tty/serial.c
@@ -61,10 +61,16 @@ static inline void delay(int32_t count) {
 #endif
 
 void serial_send(const char c) {
-	#ifdef __i386__
+	#if defined(__i386__)
 	while(!CAN_SEND);
 	outb(PORT, c);
-	#else
+
+	#elif defined(__arm__)
+	// Silly quickfix for minicom
+	if(c == '\n') {
+		serial_send('\r');
+	}
+
 	while(rpi_mmio_read(UART0_FR) & (1 << 5));
 	rpi_mmio_write(UART0_DR, c);
 	#endif
@@ -95,43 +101,5 @@ void serial_init() {
 	outb(PORT+3, 0x03);
 	outb(PORT+2, 0xC7);
 	outb(PORT+4, 0x0B);
-	#else
-	// Disable UART0.
-	rpi_mmio_write(UART0_CR, 0x00000000);
-	// Setup the GPIO pin 14 && 15.
-
-	// Disable pull up/down for all GPIO pins
-	rpi_mmio_write(GPPUD, 0x00000000);
-	delay(150);
-
-	// Disable pull up/down for pin 14,15
-	rpi_mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
-	delay(150);
-
-	// Write 0 to GPPUDCLK0 to make it take effect.
-	rpi_mmio_write(GPPUDCLK0, 0x00000000);
-
-	// Clear pending interrupts.
-	rpi_mmio_write(UART0_ICR, 0x7FF);
-
-	// Set integer & fractional part of baud rate.
-	// Divider = UART_CLOCK/(16 * Baud)
-	// Fraction part register = (Fractional part * 64) + 0.5
-	// UART_CLOCK = 3000000; Baud = 115200.
-
-	// Divider = 3000000 / (16 * 115200) = 1.627 = ~1.
-	rpi_mmio_write(UART0_IBRD, 1);
-	// Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
-	rpi_mmio_write(UART0_FBRD, 40);
-
-	// Enable FIFO & 8 bit data transmissio (1 stop bit, no parity).
-	rpi_mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
-
-	// Mask all interrupts.
-	rpi_mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
-	                       (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
-
-	// Enable UART0, receive & transfer part of UART.
-	rpi_mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
 	#endif
 }
