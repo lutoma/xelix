8e9a96ca00c0ba7008ac6366bf269b3408d7012e
Lukas Martini <hello@lutoma.org>
2019-05-18 00:10:16 +0200

    tty: Input handling improvements

--- src/tty/input.c
+++ src/tty/input.c
@@ -21,75 +21,66 @@
 #include <tty/tty.h>
 #include <tty/keymap.h>
 
-inline void write_escape(char e) {
-	term->read_buf[term->read_len++] = '\033';
-	term->read_buf[term->read_len++] = '[';
-	term->read_buf[term->read_len++] = e;
-	term->read_done = true;
+inline size_t write_escape(char* dest, char e) {
+	dest[0] = '\033';
+	dest[1] = '[';
+	dest[2] = e;
+	return 3;
 }
 
-void tty_input_cb(struct tty_input_state* input) {
-	if(!term || !input || term->read_len >= sizeof(term->read_buf)) {
-		return;
+/* Convert tty_input_state/keycodes to ASCII character. Also converts a number of
+ * single-byte escape sequences that are used in both canonical and non-canonical
+ * mode.
+ */
+static inline char convert_to_char(struct tty_input_state* input) {
+	if(input->code >= 512) {
+		return 0;
 	}
 
-	if(!(term->termios.c_lflag & ICANON)) {
-		switch(input->code) {
-			case 0x48: write_escape('A'); return;
-			case 0x50: write_escape('B'); return;
-			case 0x4d: write_escape('C'); return;
-			case 0x4b: write_escape('D'); return;
-			case 0x4f: write_escape('F'); return;
-			case 0x47: write_escape('H'); return;
-			case 0x53: write_escape('3'); return;
-			case 0x49: write_escape('5'); return;
-			case 0x51: write_escape('6'); return;
+	char* chr = &tty_keymap_en[input->code];
+	switch(*chr) {
+		case 0x8:
+		case 0x7f:
+			return term->termios.c_cc[VERASE];
+		case '\n':
+			return term->termios.c_cc[VEOL];
+	}
+
+	if(input->control_left || input->control_right) {
+		switch(*chr) {
+			case 'd': return term->termios.c_cc[VEOF];
+			case 'c': return term->termios.c_cc[VINTR];
 		}
 	}
 
-	char chr = 0;
-	if(input->code < 512) {
-		if((input->shift_left || input->shift_right) ^ input->capslock) {
-			chr = tty_keymap_en[input->code + 256];
-		} else {
-			chr = tty_keymap_en[input->code];
-		}
+	// Shift into uppercase range of keymap if necessary
+	if((input->shift_left || input->shift_right) ^ input->capslock) {
+		chr += 256;
 	}
+	return *chr;
+}
 
+/* Canonical read mode input handler. Perform internal line-editing and block
+ * read syscall until we encounter VEOF, VEOL, or the buffer is full.
+ */
+static void handle_canon(struct tty_input_state* input) {
+	char chr = convert_to_char(input);
 	if(!chr) {
 		return;
 	}
 
-	switch(chr) {
-		case 0x8:
-		case 0x7f:
-			chr = term->termios.c_cc[VERASE]; break;
-		case '\n':
-			chr = term->termios.c_cc[VEOL]; break;
+	// EOF / ^D
+	if(chr == term->termios.c_cc[VEOF]) {
+		term->read_done = true;
+		return;
 	}
 
-	if(input->control_left || input->control_right) {
-		switch(chr) {
-			case 'd':
-			case 'D':
-				if(term->termios.c_lflag & ICANON) {
-					term->read_done = true;
-					return;
-				}
-
-				chr = term->termios.c_cc[VEOF]; break;
-			case 'c':
-			case 'C':
-				if(term->fg_task && term->termios.c_lflag & ISIG) {
-					task_signal(term->fg_task, NULL, SIGINT, NULL);
-					term->fg_task->interrupt_yield = true;
-				}
-				chr = term->termios.c_cc[VINTR];
-				break;
-		}
+	// Signal task on ^C
+	if(chr == term->termios.c_cc[VINTR] && term->fg_task && term->termios.c_lflag & ISIG) {
+		task_signal(term->fg_task, NULL, SIGINT, NULL);
 	}
 
-	if(chr == term->termios.c_cc[VERASE] && term->termios.c_lflag & ICANON) {
+	if(chr == term->termios.c_cc[VERASE]) {
 		if(!term->read_len) {
 			return;
 		}
@@ -103,8 +94,57 @@ void tty_input_cb(struct tty_input_state* input) {
 		tty_write(&chr, 1);
 	}
 
-	if(chr == term->termios.c_cc[VEOL] || !(term->termios.c_lflag & ICANON) || term->read_len >= sizeof(term->read_buf)) {
+	if(chr == term->termios.c_cc[VEOL] || term->read_len >= sizeof(term->read_buf)) {
 		term->read_done = true;
+	}
+}
+
+/* Non-canonical read mode: Return immediately upon input regardless of
+ * buffer size, and also return ECMA-48 encoded versions of control characters.
+ */
+static void handle_noncanon(struct tty_input_state* input) {
+	char* inputseq;
+	char char_p[1];
+
+	size_t inputlen = 3;
+	switch(input->code) {
+		case 0x48: inputseq = "\033[A"; break;
+		case 0x50: inputseq = "\033[B"; break;
+		case 0x4d: inputseq = "\033[C"; break;
+		case 0x4b: inputseq = "\033[D"; break;
+		case 0x4f: inputseq = "\033[F"; break;
+		case 0x47: inputseq = "\033[H"; break;
+		case 0x53: inputseq = "\033[3"; break;
+		case 0x49: inputseq = "\033[5"; break;
+		case 0x51: inputseq = "\033[6"; break;
+		default:
+			*char_p = convert_to_char(input);
+			if(!*char_p) {
+				return;
+			}
+
+			inputseq = char_p;
+			inputlen = 1;
+	}
+
+	memcpy(term->read_buf, inputseq, inputlen);
+	term->read_len += inputlen;
+	if(term->termios.c_lflag & ECHO) {
+		tty_write(inputseq, inputlen);
+	}
+
+	term->read_done = true;
+}
+
+void tty_input_cb(struct tty_input_state* input) {
+	if(!term || !input || term->read_len >= sizeof(term->read_buf)) {
+		return;
+	}
+
+	if(term->termios.c_lflag & ICANON) {
+		handle_canon(input);
+	} else {
+		handle_noncanon(input);
 	}
 }
 
