6ad586f874559d1cf049e0ee941f48ece41db31a
Lukas Martini <hello@lutoma.org>
2019-06-21 22:46:43 +0200

    tasks/fork: Copy all files that have refcount, regardless of inode

--- src/tasks/task.c
+++ src/tasks/task.c
@@ -167,7 +167,7 @@ static task_t* _fork(task_t* to_fork, isf_t* state) {
 
 	memcpy(task->files, to_fork->files, sizeof(vfs_file_t) * VFS_MAX_OPENFILES);
 	for(uint32_t num = 0; num < VFS_MAX_OPENFILES; num++) {
-		if(task->files[num].inode) {
+		if(task->files[num].refs) {
 			task->files[num].task = task;
 		}
 	}
