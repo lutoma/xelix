39a4bebf5e4aaedbd887ced1be890cbd679af870
Lukas Martini <hello@lutoma.org>
2020-01-08 04:46:10 +0100

    land: Use libFLAC for xelix-utils play

--- land/xelix-utils/play.c
+++ land/xelix-utils/play.c
@@ -28,8 +28,15 @@
 #include <termios.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <FLAC/stream_decoder.h>
 
 #define CHUNK_SIZE (0x800 * 2)
+uint16_t* interleaved_buf = NULL;
+size_t interleaved_buf_size = 0;
+int dest_fd = -1;
+uint64_t cur_sample = 0;
+FLAC__StreamDecoder* decoder = NULL;
+FLAC__StreamMetadata* metadata = NULL;
 
 static inline char poll_input(int timeout) {
 	struct pollfd pfd = {
@@ -43,74 +50,119 @@ static inline char poll_input(int timeout) {
 	return getchar();
 }
 
-void playback_loop(size_t size, int source_fd, int dest_fd) {
-	// 44.1khz sample rate, s16le, 2 channels
-	int runtime = size / (44100 * 4);
+void metadata_callback(const FLAC__StreamDecoder *decoder,
+	const FLAC__StreamMetadata *_metadata, void *client_data) {
 
-	size_t total_written;
-	float lcperc = 0;
-	int lptime = 0;
-	bool pause = false;
-
-	uint8_t* buf = malloc(CHUNK_SIZE);
-
-	while(1) {
-		char input = poll_input(pause ? -1 : 0);
-		if(input) {
-			switch(input) {
-				case ' ':
-					pause = !pause;
-					break;
-			}
-			fflush(stdin);
-		}
-
-		if(!pause) {
-			if(!read(source_fd, buf, CHUNK_SIZE)) {
-				break;
-			}
-
-			total_written += write(dest_fd, buf, CHUNK_SIZE);
-		}
-
-		float cperc = (float)total_written / (float)size;
-		int ptime = total_written / (44100 * 4);
-
-		if(cperc != lcperc || ptime != lptime) {
-			printf("\033[2K\033[G %d:%02d / %d:%02d (%2.1f%%)",
-				ptime / 60, ptime % 60,
-				runtime / 60, runtime % 60,
-				cperc * 100);
-
-			if(pause) {
-				printf(" (paused)");
-			}
-
-			fflush(stdout);
-			lcperc = cperc;
-			lptime = ptime;
-		}
+	if(_metadata->type == FLAC__METADATA_TYPE_STREAMINFO) {
+		metadata = _metadata;
+		printf("%u channels, %2.1f kHz, %u bps, %llu samples\n",
+			metadata->data.stream_info.channels,
+			(float)metadata->data.stream_info.sample_rate / 1000,
+			metadata->data.stream_info.bits_per_sample,
+			metadata->data.stream_info.total_samples);
+		fflush(stdout);
 	}
-
-	free(buf);
 }
 
-void play_file(const char* path, int dest_fd) {
-	//printf("Playing: %s\n", basename(path));
-	int source_fd = open(path, O_RDONLY);
-	if(source_fd < 0) {
-		perror("Could not open");
-		return ;
+FLAC__StreamDecoderWriteStatus write_callback(const FLAC__StreamDecoder *decoder,
+	const FLAC__Frame *frame, const FLAC__int32* const buffer[], void *client_data) {
+
+	// Handle input
+	char input = poll_input(0);
+	if(input) {
+		fflush(stdout);
+		switch(input) {
+			case 'q':
+				exit(0);
+			case ' ':
+				while(poll_input(-1) != ' ');
+				break;
+		}
+		fflush(stdin);
 	}
 
-	struct stat stat;
-	if(fstat(source_fd, &stat) < 0) {
-		perror("Could not stat");
+	// Simple OSD
+	cur_sample += frame->header.blocksize;
+	if(metadata) {
+		uint32_t elapsed = (uint32_t)(cur_sample / metadata->data.stream_info.sample_rate);
+		uint32_t length = (uint32_t)(metadata->data.stream_info.total_samples / metadata->data.stream_info.sample_rate);
+		float perc = (float)cur_sample / (float)metadata->data.stream_info.total_samples;
+
+		printf("\033[2K\033[G %llu:%02u / %u:%02u (%2.1f%%)",
+			elapsed / 60, elapsed % 60,
+			length / 60, length % 60,
+			perc * 100);
+
+		fflush(stdout);
+	}
+
+	size_t bsize = sizeof(uint16_t) * frame->header.blocksize * 2;
+	if(bsize > interleaved_buf_size) {
+		if(!interleaved_buf) {
+			interleaved_buf = malloc(bsize);
+		} else {
+			interleaved_buf = realloc(interleaved_buf, bsize);
+		}
+
+		interleaved_buf_size = bsize;
+	}
+
+	int j = 0;
+	for(int i = 0; i < frame->header.blocksize; i++) {
+		interleaved_buf[i*2] = buffer[0][i];
+		interleaved_buf[i*2 + 1] = buffer[1][i];
+	}
+
+	write((int)client_data, interleaved_buf, frame->header.blocksize * 4);
+	return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
+}
+
+void error_callback(const FLAC__StreamDecoder *decoder,
+	FLAC__StreamDecoderErrorStatus status, void *client_data) {
+
+	fprintf(stderr, "Decode error: %s\n", FLAC__StreamDecoderErrorStatusString[status]);
+	exit(-1);
+}
+
+void play_file(FLAC__StreamDecoder* decoder, const char* path, int dest_fd) {
+	printf("Playing: %s\n", basename(path));
+	fflush(stdout);
+
+	cur_sample = 0;
+
+	FLAC__StreamDecoderInitStatus dc_status = FLAC__stream_decoder_init_file(
+		decoder, path, write_callback, metadata_callback,
+		error_callback, (void*)dest_fd);
+
+	if(dc_status != FLAC__STREAM_DECODER_INIT_STATUS_OK) {
+		fprintf(stderr, "Could not initialize decoder: %s\n", FLAC__StreamDecoderInitStatusString[dc_status]);
 		return;
 	}
 
-	playback_loop(stat.st_size, source_fd, dest_fd);
-	close(source_fd);
+	if(!FLAC__stream_decoder_process_until_end_of_stream(decoder)) {
+		fprintf(stderr, "Decoding failed: %s\n", FLAC__StreamDecoderStateString[FLAC__stream_decoder_get_state(decoder)]);
+		return;
+	}
+
+	FLAC__stream_decoder_finish(decoder);
+}
+
+void cleanup() {
+	if(decoder) {
+		FLAC__stream_decoder_finish(decoder);
+		FLAC__stream_decoder_delete(decoder);
+	}
+
+	if(dest_fd != -1) {
+		close(dest_fd);
+		printf("\n");
+	}
+
+	// Reset stdin
+	struct termios termios;
+	ioctl(0, TCGETS, &termios);
+	termios.c_lflag |= ICANON | ECHO;
+	ioctl(0, TCSETS, &termios);
 }
 
 int main(int argc, const char** argv) {
@@ -119,10 +171,18 @@ int main(int argc, const char** argv) {
 		return -1;
 	}
 
-	int dest_fd = open("/dev/dsp", O_WRONLY);
+	atexit(cleanup);
+
+	dest_fd = open("/dev/dsp", O_WRONLY);
 	if(dest_fd == -1) {
 		perror("Could not open device");
 		return -1;
+	}
+
+	decoder = FLAC__stream_decoder_new();
+	if(!decoder) {
+		perror("Could not allocate decoder\n");
+		return 1;
 	}
 
 	// Raw stdin
@@ -133,16 +193,8 @@ int main(int argc, const char** argv) {
 	ioctl(0, TCSETS, &termios);
 
 	for(int i = 1; i < argc; i++) {
-		play_file(argv[i], dest_fd);
+		play_file(decoder, argv[i], dest_fd);
 	}
-
-	close(dest_fd);
-	printf("\n");
-
-	// Reset stdin
-	ioctl(0, TCGETS, &termios);
-	termios.c_lflag |= ICANON | ECHO;
-	ioctl(0, TCSETS, &termios);
 
 	return 0;
 }
