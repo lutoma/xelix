e6f86cdc3637760e5df05803f04fe079a35bd1a1
Lukas Martini <hello@lutoma.org>
2025-11-06 16:16:23 +0100

    Improve buildpkgs.sh

--- util/buildpkgs.sh
+++ util/buildpkgs.sh
@@ -2,26 +2,29 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LAND_DIR="$SCRIPT_DIR/../land"
 source $SCRIPT_DIR/setenv.sh "/tmp/image"
 
 mkdir /tmp/packages || true
 
 for dir in "$@"; do
-	makedepend=()
-	depend=()
-	source "$dir/PKGBUILD"
-
-	echo -e "Building $pkgname-$pkgver: $pkgdesc\n"
-
+	# Create a new empty sysroot
 	sudo rm -rf "$SYSROOT"
 	mkdir -p "$SYSROOT/var/lib/pacman"
 
+	makedepend=()
+	depend=()
+	source "$LAND_DIR/$dir/PKGBUILD"
+	echo -e "Building $pkgname-$pkgver: $pkgdesc\n"
+
+	# Install package build dependencies to the sysroot
 	need_packages=('base' ${depend[@]} ${makedepend[@]})
-	sudo pacman --root "$SYSROOT" --noconfirm -Sy "${need_packages[@]}"
+	sudo pacman --config "$LAND_DIR/pacman/pacman.linux.conf" --root "$SYSROOT" --noconfirm -Sy "${need_packages[@]}"
 	sudo rm -vf "$SYSROOT"/usr/lib/*.la
 
-	cd $dir
-	makepkg -Ad --sign
-	mv *.pkg.tar* /tmp/packages
+	# Build package
+	cd "$LAND_DIR/$dir"
+	makepkg -fAd --sign
+	mv *.pkg.* /tmp/packages
 	cd ..
 done
